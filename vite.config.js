90f26635aa65a91ca6752be1ce97d82939e836b0
itihon <kalabin.alexandr@gmail.com>
2025-07-20 18:40:50 +0500

    chore: configure vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
-export default defineConfig({
-  root: 'src',
-  publicDir: 'public',
-  plugins: [
-    monacoEditorPlugin({
-      // Options can be specified here
-    }),
-  ],
+export default defineConfig(({ command, mode }) => {
+
+  console.log(command, mode);
+
+  if (command === 'serve-build') {
+    return {
+      root: 'dist',
+      fileName: 'index'
+    };
+  }
+  else return {
+    root: 'src',
+    publicDir: 'public',
+    plugins: [
+      monacoEditorPlugin({
+        // Options can be specified here
+      }),
+    ],
+  }
 })
